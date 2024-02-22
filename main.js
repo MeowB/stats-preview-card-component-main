ab155ae43e5bec3fbf8960c30a5d5d583b402bcd
MeowB <60074375+MeowB@users.noreply.github.com>
2024-02-22 14:23:11 +0100

    fixing some bugs and making readme

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
-window.addEventListener('resize', function() {
-    // Get the width of the window
-    var windowWidth = window.innerWidth;
+function checkWindowWidth() {
+	    // Get the width of the window
+		var windowWidth = window.innerWidth;
 
-    // Check if the window width is above 600px
-    if (windowWidth > 600) {
-        // If above 600px, change the src attribute of the image to the desktop image
-        document.querySelector('.image img').src = './images/image-header-desktop.jpg';
-    } else {
-        // If below or equal to 600px, change the src attribute of the image to the mobile image
-        document.querySelector('.image img').src = './images/image-header-mobile.jpg';
-    }
-});
+		// Check if the window width is above 600px
+		if (windowWidth > 600) {
+			// If above 600px, change the src attribute of the image to the desktop image
+			document.querySelector('.image img').src = './images/image-header-desktop.jpg';
+		} else {
+			// If below or equal to 600px, change the src attribute of the image to the mobile image
+			document.querySelector('.image img').src = './images/image-header-mobile.jpg';
+		}
+}
+
+window.addEventListener('load', checkWindowWidth);
+window.addEventListener('resize', checkWindowWidth);
